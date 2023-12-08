fd1629668e18852a0746b9fa17009ef5abb3fe90
mohammad hossein <mohammad.hajiebrahimi83@gmail.com>
2023-12-08 19:22:08 +0330

    add hh files

---
+++ magic.hh
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sep ' '
+#define endl '\n'
+#define tab '\t'
+#define all(x)(x).begin(), (x).end()
+const string MAP_PATH = "map.txt";
+const char CSV_DELIMITER = ',';
+const string WALL1 = "B";
+const string WALL2 = "P";
+const string DOOR = "D";
+const string HIDE_KEY = "K";
+const string HIDE_POWER2 = "L";
+const string HIDE_POWER3 = "S";
+const string SHOW_DOOR = "d";
+const string SHOW_KEY = "k";
+const string SHOW_POWER2 = "l";
+const string SHOW_POWER3 = "s";
+const string AGENT = "A";
+const string H_ENEMY = "H";
+const string V_ENEMY = "V";
+const string V_ENEMYDOWN = "VD";
+const string V_ENEMYUP = "VU";
+const string H_ENEMYLEFT = "HL";
+const string H_ENEMYRIGHT = "HR";
+const string EMPTY = "-";
+const string BOMB = "T";
+const string GRASS_IMAGE = "Pic/grass.png";
+const string WALL1_IMAGE = "Pic/wall-1.png";
+const string WALL2_IMAGE = "Pic/wall-2.png";
+const string V_ENEMYUP_IMAGE = "Pic/up-enemy.png";
+const string V_ENEMYDOWN_IMAGE = "Pic/down-enemy.png";
+const string H_ENEMYLEFT_IMAGE = "Pic/left-enemy.png";
+const string H_ENEMYRIGHT_IMAGE = "Pic/right-enemy.png";
+const string KEY_IMAGE = "Pic/key.png";
+const string POWER2_IMAGE = "Pic/powerup2.jpeg";
+const string POWER3_IMAGE = "Pic/powerup3.jpeg";
+const string DOOR_IMAGE = "Pic/door.jpeg";
+const string BOMB_IMAGE = "Pic/bomb.png";
+const string AGENT_UP_IMAGE = "Pic/up.png";
+const string AGENT_DOWN_IMAGE = "Pic/down.png";
+const string AGENT_LEFT_IMAGE = "Pic/left.png";
+const string AGENT_RIGHT_IMAGE = "Pic/right.png";
+typedef vector < vector < string >> VVS;
